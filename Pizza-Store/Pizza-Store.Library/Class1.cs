0163e7aa99a869b9b77e4b36524183bf4a5d3e4b
Rinkal <rdgandhi16@gmail.com>
2017-12-09 16:38:51 -0500

    First Commit after creating Library and Test Oriject

---
+++ Pizza-Store/Pizza-Store.Library/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Pizza_Store.Library
+{
+    public class Class1
+    {
+    }
+}
---
+++ Pizza-Store/Pizza-Store.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Pizza_Store.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
